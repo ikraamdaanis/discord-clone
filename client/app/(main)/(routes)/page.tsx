545d4e69dbe8e9955f9b5362aa8f9a087914c5f5
Ikraam <ibndaanis@gmail.com>
2023-08-29 03:55:55 +0100

    theme, dark and light mode toggle

--- client/app/(main)/(routes)/page.tsx
+++ client/app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { ThemeToggle } from "components/theme-toggle";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <UserButton afterSignOutUrl="/" />
+      <ThemeToggle />
     </div>
   );
 }
