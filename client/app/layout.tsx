358f7cc1b8215bea0a3533dc46267b1155a47775
Ikraam <ibndaanis@gmail.com>
2023-08-29 03:13:34 +0100

    favicon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,7 +7,7 @@ const font = Open_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Discord",
   description:
-    "Discord is the easiest way to communicate over voice, video, and text.  Chat, hang out, and stay close with your friends and communities."
+    "Discord is the easiest way to communicate over voice, video, and text. Chat, hang out, and stay close with your friends and communities."
 };
 
 export default function RootLayout({
