58edd4ee0534c63ac41cca22cdf93e225123b15b
Ikraam <ibndaanis@gmail.com>
2023-09-17 17:55:08 +0100

    file router type

--- client/app/api/uploadthing/core.ts
+++ client/app/api/uploadthing/core.ts
@@ -12,13 +12,13 @@ function handleAuth() {
 }
 
 // FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
+export const ourFileRouter: FileRouter = {
   serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
   messageFile: f(["image", "pdf"])
     .middleware(() => handleAuth())
     .onUploadComplete(() => {})
-} satisfies FileRouter;
+};
 
 export type OurFileRouter = typeof ourFileRouter;
