fe784e58cc000f2464ae78b28b0454e1f7a03eca
Ikraam <ibndaanis@gmail.com>
2023-09-21 16:27:02 +0100

    message input and  socket route

--- client/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ client/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import { redirectToSignIn } from "@clerk/nextjs";
 import { ChatHeader } from "components/chat/chat-header";
+import { ChatInput } from "components/chat/chat-input";
 import { currentProfile } from "lib/current-profile";
 import { db } from "lib/db";
 import { redirect } from "next/navigation";
@@ -41,6 +42,16 @@ const ChannelPage = async ({ params }: ChannelPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: params.serverId
+        }}
       />
     </div>
   );
