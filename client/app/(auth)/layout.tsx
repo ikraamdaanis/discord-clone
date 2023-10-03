67ed80cd2a470c4afe1f143c19474cb6569fb43b
Ikraam <ibndaanis@gmail.com>
2023-10-03 16:54:57 +0100

    login page image

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="flex h-full justify-center">{children}</div>;
+  return (
+    <div className="flex h-full items-center justify-center bg-backgroundDark bg-[url('/background.webp')]">
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
